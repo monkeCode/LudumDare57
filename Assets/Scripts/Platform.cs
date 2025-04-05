9fc19fc81f7c9f35d7029b342b723c5c51ca519f
Алексей Хоченков <alekshoch@yandex.ru>
2025-04-05 13:32:48 +0300

    papka scripts

---
+++ Assets/Scripts/Platform.cs
@@ -0,0 +1,22 @@
+using System;
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+    public int healthPoints = 100;
+
+    public float speed = 5f;
+
+    public bool isMoving = false;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
