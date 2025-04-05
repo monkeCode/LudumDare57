b4a8938ec625bf50b3d335735f47a16a7504d1c3
Сергей Бирюков <birukovsergej121@gmail.com>
2025-04-05 19:35:39 +0500

    Add currency storage

---
+++ Assets/Scripts/Currency/CurrencyStorage.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+namespace Currency
+{
+    public class CurrencyStorage: MonoBehaviour
+    {
+        private int CurrencyCount { get; set; }
+
+        public void AddCurrency(int amount)
+        {
+            CurrencyCount += amount;
+        }
+
+        public int GetCount()
+        {
+            return CurrencyCount;
+        }
+
+        public bool TrySpendCurrency(int amount)
+        {
+            if (amount > CurrencyCount) return false;
+            CurrencyCount -= amount;
+            return true;
+        }
+    }
+}
