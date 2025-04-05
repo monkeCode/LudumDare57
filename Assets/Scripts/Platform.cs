6519a830d2e78fccf00c5b09af923ac17caebc0e
Алексей Хоченков <alekshoch@yandex.ru>
2025-04-05 14:23:34 +0300

    Timer

--- Assets/Scripts/Platform.cs
+++ Assets/Scripts/Platform.cs
@@ -7,16 +7,18 @@ public class Platform : MonoBehaviour
 
     public float speed = 5f;
 
-    public bool isMoving = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Timer.platformRiding)
+        {
+            transform.position -= new Vector3(0, speed, 0) * Time.deltaTime;
+        }
     }
 }
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using System.Collections;
+
+public class Timer : MonoBehaviour
+{
+    public static Timer instance = null;
+    public static bool platformRiding = false;
+
+    public static float timeForLooting = 60f;
+
+    public static float timeForFighting = 45f;
+
+    private IEnumerator coroutine;
+
+    void Start()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance == this)
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+        coroutine = Time(timeForLooting, timeForFighting);
+        StartCoroutine(coroutine);
+    }
+
+    private IEnumerator Time(float timeChill, float timeFight)
+    {
+        while (true)
+        {
+            if (platformRiding)
+            {
+                yield return new WaitForSeconds(timeChill);
+                platformRiding = !platformRiding;
+            }
+            else
+            {
+                yield return new WaitForSeconds(timeFight);
+                platformRiding = !platformRiding;
+            }
+        }
+    }
+}
