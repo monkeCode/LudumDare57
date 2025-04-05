b098e30e87233257de67e15d00b3c6c37abbc71d
Сергей Бирюков <birukovsergej121@gmail.com>
2025-04-05 20:27:26 +0500

    implement IDamagable for MineralSource

--- Assets/Scripts/GameResources/MineralSource.cs
+++ Assets/Scripts/GameResources/MineralSource.cs
@@ -1,34 +1,38 @@
+using Interfaces;
 using UnityEngine;
 
 namespace GameResources
 {
-    public class MineralSource: MonoBehaviour
+    public class MineralSource: MonoBehaviour, IDamageable
     {
-        [field: SerializeField]private int Durability { get; set; } = 1;
-        [field: SerializeField] private int MineralsCount { get; set; } = 1;
+        [field: SerializeField] private uint Durability { get; set; } = 1;
+        [field: SerializeField] private uint MineralsCount { get; set; } = 1;
         [field: SerializeField] private Mineral[] MineralPrefabs { get; set; }
 
-        private void OnHit()
+        private Vector2 GetRandomPosition(Vector2 startPosition)
         {
-            Durability -= 1;
+            return new Vector2(startPosition.x + Random.Range(-5, 5), startPosition.y + Random.Range(-5, 5));  
+        }
+
+        public void TakeDamage(uint damage)
+        {
+            Durability -= damage;
             if (Durability <= 0)
             {
-                Destroy();
+                Kill();
             }
         }
 
-        private void Destroy()
+        // Can't heal
+        public void Heal(uint heals) { }
+
+        public void Kill()
         {
             for (int i = 0; i < MineralsCount; i++)
             {
                 Instantiate(MineralPrefabs[Random.Range(0, MineralPrefabs.Length)], GetRandomPosition(transform.position), transform.rotation);   
             }
             Destroy(gameObject);
-        }
-
-        private Vector2 GetRandomPosition(Vector2 startPosition)
-        {
-            return new Vector2(startPosition.x + Random.Range(-5, 5), startPosition.y + Random.Range(-5, 5));  
         }
     }
 }
