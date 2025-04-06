81096c77b5811fc530576359d6d3ff2c9513fa1f
Алексей Хоченков <alekshoch@yandex.ru>
2025-04-06 18:59:33 +0300

    chto-to

---
+++ Assets/Scripts/UI/GameplayUI/AmmoPlayer.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+using TMPro;
+using UnityEngine;
+
+public class AmmoPlayer : MonoBehaviour
+{
+    public TMP_Text healthText;
+
+    private Player.Player _player;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        _player = FindFirstObjectByType<Player.Player>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        healthText.text = $"{_player.Hp}/{_player.MaxHp}";
+    }
+}
+
